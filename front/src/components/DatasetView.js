a55534a8b01ec198eb8852ea88223cd3b87ff379
Kris <kris.fillmore@gmail.com>
2021-06-19 22:34:38 -0700

    test data function and some css

--- front/src/components/DatasetView.js
+++ front/src/components/DatasetView.js
@@ -1,18 +1,34 @@
-import {React, useState} from 'react'
+import { React, useState, useEffect } from "react";
+import "./DatasetView.css"
 
 function DatasetView() {
-    const [datasetId, setDatasetId] = useState(0);
-    const [datasetName, setDatasetName] = useState("");
-    const [datasetType, setDatasetType] = useState("");
-    const [datasetSubtype, setDatasetSubtype] = useState("");
-    const [datasetItems, setDatasetItems] = []
+  const [datasetId, setDatasetId] = useState(0);
+  const [datasetName, setDatasetName] = useState("");
+  const [datasetType, setDatasetType] = useState("");
+  const [datasetSubtype, setDatasetSubtype] = useState("");
+  const [datasetItems, setDatasetItems] = [];
 
+  useEffect(() => {
+    loadData();
+  }, []);
+  function loadData() {
+    setDatasetId(0);
+    setDatasetName("TESTNAME");
+    setDatasetType("Image");
+    setDatasetSubtype("Clasification");
+  }
 
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div className="DatasetDetails">
+        <div>ID: {datasetId}</div>
+        <div>Name: {datasetName}</div>
+        <div>Type: {datasetType}</div>
+        <div>Problem: {datasetSubtype}</div>
+      </div>
+      <div>DatasetTableComponent</div>
+    </div>
+  );
 }
 
-export default DatasetView
+export default DatasetView;
