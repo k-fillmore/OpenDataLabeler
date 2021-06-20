4d66757cb786a9b9c9ee3741c7ae4e6f22d49890
Kris <kris.fillmore@gmail.com>
2021-06-19 21:55:43 -0700

    datasetview component init

---
+++ front/src/components/DatasetView.js
@@ -0,0 +1,18 @@
+import {React, useState} from 'react'
+
+function DatasetView() {
+    const [datasetId, setDatasetId] = useState(0);
+    const [datasetName, setDatasetName] = useState("");
+    const [datasetType, setDatasetType] = useState("");
+    const [datasetSubtype, setDatasetSubtype] = useState("");
+    const [datasetItems, setDatasetItems] = []
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DatasetView
