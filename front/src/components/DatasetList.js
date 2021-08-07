4179708fbabea273414400e46c4f7bc9ebcb9e59
Kris <kris.fillmore@gmail.com>
2021-08-06 20:16:22 -0700

    create dataset list component

--- front/src/components/DatasetList.js
+++ front/src/components/DatasetList.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import { React, useState, useEffect } from "react";
+import request from "superagent";
+import { Card, Button } from "react-bootstrap";
+import "./DatasetList.css";
 
-function Datasets() {
-    //remember to make this try and load something with useEffect
-    const [datasetList, setDatasetList] = useState([])
-    
-
-    return (
-        <div>
-            
-        </div>
-    )
+function DatasetList() {
+  let [datasets, setDatasets] = useState([]);
+  useEffect(
+    () =>
+      request.get("http://localhost:8000/api/allDatasets").then((data) => {
+        setDatasets(data.body);
+        console.log(data.body);
+      }),
+    []
+  );
+  function generateCards() {
+    return datasets.map((dataset) => {
+      return (
+        <Card key={dataset.id} style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>{dataset.name}</Card.Title>
+            <Card.Text>
+              {dataset.kind + " " + dataset.problem}
+            </Card.Text>
+            <Button variant="dark">Label Data</Button>
+          </Card.Body>
+        </Card>
+      );
+    });
+  }
+  return (
+    <div className="datasetcards">
+      {generateCards()}
+    </div>
+  );
 }
 
-export default Datasets
+export default DatasetList;
